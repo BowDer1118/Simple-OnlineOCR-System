27e66bc40ffc0dad51402d7a0ce3ae53f0c32517
BowDer1118 <blue1118.ss@gmail.com>
2023-04-30 14:33:23 +0800

    Update websocket_server.py

--- Program/WebSocket Server/websocket_server.py
+++ Program/WebSocket Server/websocket_server.py
@@ -24,56 +24,100 @@ def display_message(message: str):
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
     print(f"[{current_time}]: {message}")
 
+async def handle_handshake(websocket,message_json):
+    # 生成一個帶有時間資訊的 uuid 字串
+    current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    client_uuid = f"{current_time}-{uuid.uuid4()}"
+
+    # 創建一個 ClientInfo 物件
+    client_info = ClientInfo(websocket,message_json['ClientName'], client_uuid, set())
+
+    # 將 uuid 和 ClientInfo 物件加入到 clients 字典中
+    clients[client_uuid] = client_info
+
+    # 組裝要傳送的 JSON 物件
+    data={"ClientUUID": client_uuid}
+
+    # 顯示已接受客戶端的連線請求
+    display_message(f"已接受客戶端名稱[{client_info.client_name}]的連線請求，並分配給客戶端UUID[{client_uuid}]")
+
+    return data
+
+async def temp_ocr_response(image_file_name):
+    # ----假裝處理完畢(為了能讓客戶端與伺服器繼續互動)----
+    # 組裝要傳送的 JSON 物件
+    data={
+        "ResponseMessage": "SUCCESS",
+        "ImageFileName": image_file_name,
+        "OcrResult": "假裝完成 OCR 任務了"
+    }
+    display_message(f"已經將圖片[{image_file_name}]的處理結果傳送給客戶端。")
+    return data
+
+async def handle_image_upload(message_json):
+    
+    client=clients[message_json['ClientUUID']]
+    
+    #將圖片名稱加入image_file_names中
+    image_file_name=message_json['ImageFileName']
+    client.image_file_names.add(image_file_name)
+
+    display_message(f"已收到來自客戶端名稱[{client.client_name}]的[{image_file_name}]圖片")
+
+    #真正的handle_image_upload任務:發送一個JSON到RabbitMQ中的pending queue中
+    # 尚未完成的程式碼...
+
+    #產生handle_image_upload處理完畢後，要返回給客戶端的資料
+    data={
+        "ResponseMessage": "PENDING",
+        "ImageFileName": image_file_name,
+        "OcrResult": "尚未得到OCR結果"
+    }
+    display_message(f"已將圖片資訊傳到RabbitMQ伺服器!")
+    
+
+    #為了要讓伺服器與客戶端可以通訊，先暫時使用temp_ocr_response的data當作要返回給客戶端的資料
+    data = await temp_ocr_response(image_file_name)
+
+    return data
+
+async def handle_websocket_error(websocket,error_message):
+    # 尋找客戶端的連線，並移除客戶端的ClientInfo
+    for client_uuid, client_info in list(clients.items()): # 從所有鍵值對尋找
+        if client_info.websocket == websocket:
+            display_message(f"伺服器與客戶端名稱[{client_info.client_name}]斷開連線，原因:{error_message}")
+            del clients[client_uuid] # 刪除元素
+            break
+
 # 建立 WebSocket 伺服器處理函式
 async def server(websocket, path):
     try:
         # 無窮迴圈，讓伺服器持續運作
         while True:
-            # 從 WebSocket 接收訊息
-            message = await websocket.recv()
-
-            # 乘載 JSON 物件
-            data = None
-
-            # 將訊息轉換為 JSON
-            message_json = json.loads(message)
-
-            # 如果收到的JSON資料只有一個key-value，代表是握手請求。
-            if len(message_json) == 1:
-                # 生成一個帶有時間資訊的 uuid 字串
-                current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-                client_uuid = f"{current_time}-{uuid.uuid4()}"
-
-                # 創建一個 ClientInfo 物件
-                client_info = ClientInfo(websocket,message_json['ClientName'], client_uuid, set())
-
-                # 將 uuid 和 ClientInfo 物件加入到 clients 字典中
-                clients[client_uuid] = client_info
-                # 組裝要傳送的 JSON 物件
-                data={"ClientUUID": client_uuid}
-
-                # 顯示已接受客戶端的連線請求
-                display_message(f"已接受客戶端名稱[{client_info.client_name}]的連線請求，並分配給客戶端UUID[{client_uuid}]")
-
-            # 如果收到圖片
-            elif len(message_json) == 3:
-                client=clients[message_json['ClientUUID']]
-                display_message(f"已收到來自客戶端名稱[{client.client_name}]的[{message_json['ImageFileName']}]圖片")
-                # 組裝要傳送的 JSON 物件
-                data={
-                    "ResponseMessage": "SUCCESS",
-                    "ImageFileName": message_json['ImageFileName'],
-                    "OcrResult": "假裝完成 OCR 任務了"
-                }
-            # 回傳處理結果
-            await websocket.send(json.dumps(data))
-    except Exception as e:
-        # 尋找客戶端的連線，並移除客戶端的ClientInfo
-        for client_uuid, client_info in list(clients.items()): # 從所有鍵值對尋找
-            if client_info.websocket == websocket:
-                display_message(f"客戶端名稱[{client_info.client_name}]主動斷開連線")
-                del clients[client_uuid] # 刪除元素
+            try:
+                # 從 WebSocket 接收訊息，最長等待30秒，超時會引發異常
+                message = await asyncio.wait_for(websocket.recv(), timeout=30)
+                # 將訊息轉換為 JSON
+                message_json = json.loads(message)
+                
+                # 一個字典，用來裝伺服器要回應給客戶端的JSON
+                data = None
+                
+                # 如果收到的JSON資料只有一個key-value，代表是握手請求。
+                if len(message_json) == 1:
+                    data = await handle_handshake(websocket,message_json)
+                # 如果收到的JSON資料有3個key-value，代表收到上傳的圖片
+                elif len(message_json) == 3:
+                    data = await handle_image_upload(message_json)
+                # 回傳處理結果
+                await websocket.send(json.dumps(data))
+            except asyncio.TimeoutError:
+                # 如果等待30秒仍未接收到訊息，中斷連線並處理錯誤
+                await handle_websocket_error(websocket,"已等待客戶端30秒，但仍未收到客戶端的訊息。")
+                await websocket.close()
                 break
+    except Exception as e: #處理意外狀況
+        await handle_websocket_error(websocket,"通訊發生意外狀況，客戶端可能已經主動關閉連線。")
 
 if __name__=='__main__':
     # 讀取設定檔
